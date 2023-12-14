a1d627ff35d164d74fd07393d50cb2c8f22bc5fd
lyn <Lyndon_lilin@123.com>
2023-12-14 15:11:02 +0800

    style: menu pages style

--- src/pages/admin/views/menu/menu1/menu12/menu121/index.vue
+++ src/pages/admin/views/menu/menu1/menu12/menu121/index.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <el-input v-model="val" placeholder="menu121：请输入内容测试路由缓存"></el-input>
+  <div class="app-container">
+    <div class="table-search">
+      <el-form inline>
+        <el-form-item :label="label">
+          <el-input
+            v-model="val"
+            :placeholder="`${label} 请输入内容测试路由缓存`"
+            type="textarea"
+            :autosize="{ minRows: 4, maxRows: 10 }"
+            style="width: 300px"
+          />
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -9,6 +21,7 @@ export default {
   name: 'Menu121',
   data() {
     return {
+      label: 'Menu121',
       val: ''
     }
   }
